68a9474846fd73a693d9ad75b145888e5b330221
Rob Nelson <guruvan@maza.club>
2015-09-18 04:14:31 +0000

    add template

---
+++ template/app/src/chains/XXXX_coin.py
@@ -0,0 +1,13 @@
+import cryptocur
+
+class Currency(cryptocur.CryptoCur):
+    p2pkh_version = 52
+    #pubkey_addr
+    p2sh_version = 13
+    # script_addr
+    genesis_hash = '000000000062b72c5e2ceb45fbc8587e807c155b0da735e6483dfba2f0a9c770'
+
+    coin_name = 'XXXX_COIN'
+    code = 'XXXX_SYM'
+
+    irc_channel = '#electrum-XXXX_SYM'
---
+++ template/new_coin.sh
@@ -0,0 +1,14 @@
+
+XXXX_SYM=${1}
+XXXX_COIN=${2}
+XXXX_RPCPORT=${3}
+XXXX_P2PPORT=${4}
+XXXX_company=${5}
+FILES="app/encompass-mercury.conf app/start.sh etc/service/encompass-mercury/run etc/encompass-mercury.conf app/src/chains/XXXX_COIN.py"
+for file in ${FILES} ; do
+  sed -i -e 's/XXXX_SYM/'${XXXX_SYM}'/g' ${file}
+  sed -i -e 's/XXXX_COIN/'${XXXX_COIN}'/g' ${file}
+  sed -i -e 's/XXXX_RPCPORT/'${XXXX_RPCPORT}'/g' ${file}
+  sed -i -e 's/XXXX_P2PPORT/'${XXXX_P2PPORT}'/g' ${file}
+  sed -i -e 's/XXXX_company/'${XXXX_company}'/g' ${file}
+done
