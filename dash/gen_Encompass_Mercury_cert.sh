5813b1a0bee0bc6022bdff18ff962a102900a60a
Rob Nelson <guruvan@maza.club>
2015-10-02 19:12:22 +0000

    Add new Variables, cert generators to DASH

---
+++ dash/gen_Encompass_Mercury_cert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+dev="-dev"
+COIN_SYM=${COIN_SYM:-mzc}
+IMAGE=${IMAGE:-mazaclub/encompass-mercury:${COIN_SYM}${dev}}
+HOST_DATA_PREFIX=${HOST_DATA_PREFIX:-/opt/tmp/mercury/${COIN_SYM}/mercury-disk/}
+
+
+docker run -it --rm \
+  --name mercury_gencert \
+  -e COIN=${COIN} \
+  -e COIN_SYM=${COIN_SYM} \
+  -v ${HOST_DATA_PREFIX}/app/certs:/app/certs \
+  ${IMAGE} /app/gen_cert.sh
