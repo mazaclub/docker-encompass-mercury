491ae9a006c295dacee4fd0af518d03a3a2a59d4
Rob Nelson <guruvan@maza.club>
2015-10-03 03:09:10 +0000

    Update variables / gen_cert scripts for nmc

--- nmc/app/start.sh
+++ nmc/app/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Mazacoind is expected to be at hostname 'namecoind'
+# namecoind is expected to be at hostname 'namecoind'
 # which is set via --link
 # Because we use linked containers we can use the 
 # standard ports 
@@ -12,14 +12,61 @@ ENCOMPASS_MERCURY_PORT=${ENCOMPASS_MERCURY_PORT:-50001}
 ENCOMPASS_MERCURY_SSLPORT=${ENCOMPASS_MERCURY_SSL_PORT:-50002}
 COIND=${COIND:-localhost}
 COINDIR=${COINDIR:-/home/${USER}/.${COIN}}
-RPCPORT=${RPCPORT:-8336}
+RPCPORT=${RPCPORT:-8888}
 RPCUSER=${RPCUSER:-$(grep rpcuser "${COINDIR}"/${COIN}.conf |awk -F= '{print $2}')}
 RPCPASSWORD=${RPCPASSWORD:-$(grep rpcpassword "${COINDIR}"/${COIN}.conf |awk -F= '{print $2}')}
 txidx=$(grep "txindex=" "${COINDIR}"/${COIN}.conf |awk -F= '{print $2}')
 TXINDEX=${TXINDEX:-${txidx}}
 ENCOMPASS_MERCURY_PASSWORD=$(egrep '^password =' /etc/encompass-mercury.conf|awk -F= '{print $2}')
-ENCOMPASS_MERCURY_IRCNICK=${ENCOMPASS_MERCURY_IRCNICK:-${COIN}_mazaclub}
+ENCOMPASS_MERCURY_IRCNICK=${ENCOMPASS_MERCURY_IRCNICK:-$[ 1 + $[RANDOM % 99999 ]]__mazaclub}
+ENCOMPASS_MERCURY_BLOCK_CACHE_SIZE=${ENCOMPASS_MERCURY_BLOCK_CACHE_SIZE:-120}
+ENCOMPASS_MERCURY_PRUNING_LIMIT=${ENCOMPASS_MERCURY_PRUNING_LIMIT:-10000}
+ENCOMPASS_MERCURY_DONATION_ADDR=${ENCOMPASS_MERCURY_DONATION_ADDR:-MPXEVRtXTBrz6xfn9iCyzK7Kr8P69oZjyq}
+ENCOMPASS_MERCURY_REPORT_HOST=${ENCOMPASS_MERCURY_REPORT_HOST:-${ENCOMPASS_MERCURY_HOSTNAME}}
+ENCOMPASS_MERCURY_OUTSIDE_TCP_PORT=${ENCOMPASS_MERCURY_OUTSIDE_TCP_PORT:-${ENCOMPASS_MERCURY_TCP_PORT}}
+ENCOMPASS_MERCURY_OUTSIDE_SSL_PORT=${ENCOMPASS_MERCURY_OUTSIDE_SSL_PORT:-${ENCOMPASS_MERCURY_SSL_PORT}}
+ENCOMPASS_MERCURY_CERT_FILE=${ENCOMPASS_MERCURY_CERT_FILE:-/app/certs/encompass-mercury-${COIN_SYM}.crt}
+ENCOMPASS_MERCURY_KEY_FILE=${ENCOMPASS_MERCURY_KEY_FILE:-/app/certs/encompass-mercury-${COIN_SYM}.key}
 
+
+if [ ! -f ${ENCOMPASS_MERCURY_KEY_FILE}  ] ; then
+   echo "${ENCOMPASS_MERCURY_KEY_FILE} not found"
+   echo "Refusing to start with out a cert key"
+   echo "Make a new key with /app/gen_cert.sh"
+   echo "If this server was in operation and you regenerate your certificat"
+   echo "clients will refuse to connect via SSL unless the remove old cert info"
+   echo "To temporarily stop the encompass-mercury service:"
+   echo "touch /etc/service/encompass-mercury/down"
+   echo "Either shut down this container and run the host-level shell script"
+   echo "to run this image and create a cert for normal operation of this image"
+   echo "or run /app/gen_cert.sh directly - ensure that you've mapped /app/certs"
+   echo "correctly so that you can save your new certificate."
+   exit 55
+fi
+if [ ! -f ${ENCOMPASS_MERCURY_CERT_FILE} ] ; then
+      echo "Don't see your crt as .crt or .pem file in /app/certs"
+      echo "If this server was in operation and you regenerate your certificat"
+      echo "clients will refuse to connect via SSL unless the remove old cert info"
+      touch /etc/service/encompass-mercury/down
+      echo "Either shut down this container and run the host-level shell script"
+      echo "to run this image and create a cert for normal operation of this image"
+      echo "or run /app/gen_cert.sh directly - ensure that you've mapped /app/certs"
+      echo "correctly so that you can save your new certificate."
+      exit 54
+fi
+cert_host=$( openssl x509 -in ${ENCOMPASS_MERCURY_CERT_FILE} -text -noout |grep Subject |grep CN|awk 'BEGIN{FS="/|=|,"; OFS="\t"}{print $12}')
+if [ "${cert_host}" != "${ENCOMPASS_MERCURY_REPORT_HOST}" ] ; then
+   echo "Your externl name and your SSL certificate don't match"
+      echo "If this server was in operation and you regenerate your certificat"
+      echo "clients will refuse to connect via SSL unless the remove old cert info"
+      touch /etc/service/encompass-mercury/down
+      echo "Either shut down this container and run the host-level shell script"
+      echo "to run this image and create a cert for normal operation of this image"
+      echo "or run /app/gen_cert.sh directly - ensure that you've mapped /app/certs"
+      echo "correctly so that you can save your new certificate."
+      exit 50
+fi
+ 
 
 test -z $ENCOMPASS_MERCURY_PASSWORD && ENCOMPASS_MERCURY_PASSWORD=$(apg -a 0 -m 32 -x 32 -n 1)
 if [ "${TXINDEX}" = "1" ] ; then
@@ -43,7 +90,26 @@ IFS="" sed -e 's/coind_host\ \=.*/coind_host\ \=\ '${COIND}'/g' \
 	-e 's/^stratum_tcp_ssl_port\ \=.*/stratum_tcp_ssl_port\ \=\ '${ENCOMPASS_MERCURY_SSLPORT}'/g' \
 	-e 's/^stratum_tcp_port\ \=.*/stratum_tcp_port\ \=\ '${ENCOMPASS_MERCURY_PORT}'/g' \
         -e 's/^irc_nick\ \=.*/irc_nick\ \=\ '${ENCOMPASS_MERCURY_IRCNICK}'/g' \
-	encompass-mercury.conf > /tmp/new-encompass-mercury.conf
+        -e 's/^pruning_limit\ \=.*/pruning_limit\ \=\ '${ENCOMPASS_MERCURY_PRUNING_LIMIT}'/g' \
+        -e 's/^block_cache_size\ \=.*/block_cache_size\ \=\ '${ENCOMPASS_MERCURY_BLOCK_CACHE_SIZE}'/g' \
+        -e 's/^report_stratum_tcp_ssl_port\ \=.*/report_stratum_tcp_ssl_port\ \=\ '${ENCOMPASS_MERCURY_OUTSIDE_SSL_PORT}'/g' \
+        -e 's/^report_stratum_tcp_port\ \=.*/report_stratum_tcp_port\ \=\ '${ENCOMPASS_MERCURY_OUTSIDE_TCP_PORT}'/g' \
+        -e 's/^report_host\ \=.*/report_host\ \=\ '${ENCOMPASS_MERCURY_REPORT_HOST}'/g' \
+        -e 's|^ssl_certfile\ \=.*|ssl_certfile\ \=\ '${ENCOMPASS_MERCURY_CERT_FILE}'|g' \
+        -e 's|^ssl_keyfile\ \=.*|ssl_keyfile\ \=\ '${ENCOMPASS_MERCURY_KEY_FILE}'|g' \
+        encompass-mercury.conf > /tmp/new-encompass-mercury.conf
 cp /tmp/new-encompass-mercury.conf /etc/encompass-mercury.conf
+shopt -s nocasematch
+if [[ "${ENCOMPASS_MERCURY_IRC_OVERRIDE}" = "true" ]]; then
+  test -z ${ENCOMPASS_MERCURY_IRC_CHANNEL} \
+   || sed -e 's/^irc_channel\ \=.*/irc_channel\ \=\ '${ENCOMPASS_MERCURY_IRC_CHANNEL}'/g' \
+           /app/src/chains/${COIN}.py > /tmp/new_${COIN}.py
+           mv /tmp/new_${COIN}.py /app/src/chains/${COIN}.py
+  test -z ${ENCOMPASS_MERCURY_IRCNICK_PREFIX} \
+   || sed -e 's/^irc_nick_prefix\ \=.*/irc_nick_prefix\ \=\ '${ENCOMPASS_MERCURY_IRCNICK_PREFIX}'/g' \
+           /app/src/chains/${COIN}.py > /tmp/new_${COIN}.py
+           mv /tmp/new_${COIN}.py /app/src/chains/${COIN}.py
+shopt -u nocasematch
+fi
 exec /app/run_encompass_mercury --coin ${COIN_SYM}
-	#-e 's/^password\ \=.*/password\ \=\ '${ENCOMPASS_MERCURY_PASSWORD}'/g' \
+
--- nmc/start_Encompass-Mercury.sh
+++ nmc/start_Encompass-Mercury.sh
@@ -1,30 +1,116 @@
 #!/bin/bash -x
-#######
 ###### This provides a generic example to start an Encompass-Mercury server for a coin
-IMAGE="mazaclub/encompass-mercury:nmc"
-GROUP="encompass-mercury"
-APP="encompass-mercury"
-HOST_DATA_PREFIX="/opt/data/encompass-mercury"
-COINDIR="/home/coin/.${COIN}"
-DATA_VOLDIR="/var/encompass-mercury"
-HOSTNAME="nmc.mercury.maza.club"
-NAME="${GROUP}_${APP}"
-
-#check_data () {
-# docker ps -a |grep ${GROUP}_${APPDATA} || docker run -d --name=${GROUP}_${APPDATA} -v ${DATA_VOLDIR}  ${DATA}
+# Variables are given with defaults! 
+# Either set variables on command line overriding default in this script:
+#     USER=coin ENCOMPASS_MERCURY_IRCNICK=mazaclub ./start_Encompass-Mercury.sh 
+# OR
+# change the default in this script
+#     ENCOMPASS_MERCURY_IRCNICK=${ENCOMPASS_MERCURY_IRCNICK:-mazaclub}}
+#     ->>  ENCOMPASS_MERCURY_IRCNICK=${ENCOMPASS_MERCURY_IRCNICK:-metalurgist}}
+#     OR   ENCOMPASS_MERCURY_IRCNICK=metalurgist
 #
+# 
+
+###########################
+### Variables supported by mazaclub/${COIND}-base images
+###  - these are used INSIDE the docker container, 
+###    and provided to it via -e ENV=var in docker run statements
+###  - these variables are required to be set
+###  - TXINDEX=1 is required for Encompass-Mercury operation
+###  - it is HIGHLY recommended to NOT REINDEX your blockchain 
+###    and instead download a fresh copy with TXINDEX=1 set
+
+COIN=${COIN:-bitcoin}
+COIN_SYM=${COIN_SYM:-nmc}
+COIND=${COIND:-localhost}
+RPCPORT=${RPCPORT:-8332}
+COINDIR=${COINDIR:-/home/${USER}/.${COIN}}
+TXINDEX=${TXINDEX:-1}
+
+###############################################
+## Variables for start_Encompass-Mercury.sh 
+##  - these are NOT used inside the container, these are 
+##    only used to START the container
+
+IMAGE="${IMAGE:-mazaclub/encompass-mercury:${COIN_SYM}}"
+GROUP="mercury"
+APP="${COIN}"
+HOST_DATA_PREFIX="/opt/data/encompass-mercury"
+DATA_VOLDIR="/var/encompass-mercury/${COIN_SYM}"
+HOSTNAME="${COIN_SYM}.mercury.${DOMAIN}"
+NAME="${GROUP}_${APP}"
+#################################
+
+#################################
+## Variables supported in /app/start.sh
+###  - these are used INSIDE the docker container, 
+###    and provided to it via -e ENV=var in docker run statements
+###  - thes show their default values in /app/start.sh
+###  - used to configure encompass-mercury.conf
+###  - If you are behind NAT, and are connecting your server to IRC
+###    ENCOMPASS_MERCURY_REPORT_HOST
+###    ENCOMPASS_MERCURY_OUTSIDE_TCP_PORT
+###    ENCOMPASS_MERCURY_OUTSIDE_SSL_PORT
+###    are needed to provide IRC clients with your correct public DNS/ports
+
+#ENCOMPASS_MERCURY_PRUNING_LIMIT=${ENCOMPASS_MERCURY_PRUNING_LIMIT:-10000}
+#ENCOMPASS_MERCURY_HOSTNAME=${ENCOMPASS_MERCURY_HOSTNAME:-${HOSTNAME}}
+#ENCOMPASS_MERCURY_PORT=${ENCOMPASS_MERCURY_PORT:-50001}
+#ENCOMPASS_MERCURY_SSLPORT=${ENCOMPASS_MERCURY_SSL_PORT:-50002}
+#RPCUSER=${RPCUSER:-$(grep rpcuser "${COINDIR}"/${COIN}.conf |awk -F= '{print $2}')}
+#RPCPASSWORD=${RPCPASSWORD:-$(grep rpcpassword "${COINDIR}"/${COIN}.conf |awk -F= '{print $2}')}
+#ENCOMPASS_MERCURY_PASSWORD=$(egrep '^password =' /etc/encompass-mercury.conf|awk -F= '{print $2}')
+#ENCOMPASS_MERCURY_IRCNICK=${ENCOMPASS_MERCURY_IRCNICK:-$[ 1 + $[RANDOM % 99999 ]]__mazaclub}
+#ENCOMPASS_MERCURY_BLOCK_CACHE_SIZE=${ENCOMPASS_MERCURY_BLOCK_CACHE_SIZE:-120}
+#ENCOMPASS_MERCURY_DONATION_ADDR=${ENCOMPASS_MERCURY_DONATION_ADDR:-1CnCRnTLW1uQaFWguRsvmQJXWA3G9nfa9T}
+#ENCOMPASS_MERCURY_REPORT_HOST=${ENCMPASS_MERCURY_REPORT_HOST:-${ENCOMPASS_MERCURY_HOSTNAME}}
+#ENCOMPASS_MERCURY_OUTSIDE_TCP_PORT=${ENCOMPASS_MERCURY_OUTSIDE_TCP_PORT:-${ENCOMPASS_MERCURY_PORT}}
+#ENCOMPASS_MERCURY_OUTSIDE_SSL_PORT=${ENCOMPASS_MERCURY_OUTSIDE_SSL_PORT:-${ENCOMPASS_MERCURY_SSL_PORT}}
+#######################################
+
+
+
+ENCOMPASS_MERCURY_PRUNING_LIMIT=${ENCOMPASS_MERCURY_PRUNING_LIMIT:-10000}
+ENCOMPASS_MERCURY_HOSTNAME=${ENCOMPASS_MERCURY_HOSTNAME:-nmc.mercury.docker.local}
+ENCOMPASS_MERCURY_PORT=${ENCOMPASS_MERCURY_PORT:-50001}
+ENCOMPASS_MERCURY_SSL_PORT=${ENCOMPASS_MERCURY_SSL_PORT:-50002}
+ENCOMPASS_MERCURY_OUTSIDE_TCP_PORT=${ENCOMPASS_MERCURY_OUTSIDE_TCP_PORT:-50001}
+ENCOMPASS_MERCURY_OUTSIDE_SSL_PORT=${ENCOMPASS_MERCURY_OUTSIDE_SSL_PORT:-50002}
+ENCOMPASS_MERCURY_IRCNICK=${ENCOMPASS_MERCURY_IRCNICK:-dev_mazaclub}
+ENCOMPASS_MERCURY_BLOCK_CACHE_SIZE=${ENCOMPASS_MERCURY_BLOCK_CACHE_SIZE:-120}
+ENCOMPASS_MERCURY_DONATION_ADDR=${ENCOMPASS_MERCURY_DONATION_ADDR:-1CnCRnTLW1uQaFWguRsvmQJXWA3G9nfa9T}
+ENCOMPASS_MERCURY_REPORT_HOST=${ENCMPASS_MERCURY_REPORT_HOST:-nmc.mercury.maza.club}
+
+
 
 run () {
-#check_data
 docker run -d \
   -h "${HOSTNAME}" \
   --name=${NAME} \
   --restart=always \
-  -p 50002:50002 \
-  -p 8000:8000 \
-  -p 9347:9347 \
-  -v ${HOST_DATA_PREFIX}/${COIN}:${COINDIR}
-  -v ${HOST_DATA_PREFIX}/encompass-mercury:${DATA_VOLDIR}
+  -p ${ENCOMPASS_MERCURY_OUTSIDE_SSL_PORT}:${ENCOMPASS_MERCURY_SSL_PORT} \
+  -p ${ENCOMPASS_MERCURY_OUTSIDE_TCP_PORT}:${ENCOMPASS_MERCURY_PORT} \
+  -p ${ENCOMPASS_MERCURY_OUTSIDE_DAEMON_PORT}:${ENCOMPASS_MERCURY_DAEMON_PORT} \
+  -p ${OUTSIDE_RPCPORT}:${RPCPORT} \
+  -v ${HOST_DATA_PREFIX}/${COIN}:${COINDIR} \
+  -v ${HOST_DATA_PREFIX}/encompass-mercury:${DATA_VOLDIR} \
+  -v ${HOST_DATA_PREFIX}/log:/var/log/encompass-mercury-${COIN}.log \
+  -e COIN=${COIN} \
+  -e COIN_SYM=${COIN_SYM} \
+  -e COIND=${COIND} \
+  -e RPCPORT=${RPCPORT} \
+  -e COINDIR=${COINDIR} \
+  -e TXINDEX=${TXINDEX} \
+  -e ENCOMPASS_MERCURY_PRUNING_LIMIT=${ENCOMPASS_MERCURY_PRUNING_LIMIT} \
+  -e ENCOMPASS_MERCURY_HOSTNAME=${ENCOMPASS_MERCURY_HOSTNAME} \
+  -e ENCOMPASS_MERCURY_PORT=${ENCOMPASS_MERCURY_PORT} \
+  -e ENCOMPASS_MERCURY_SSL_PORT=${ENCOMPASS_MERCURY_SSL_PORT} \
+  -e ENCOMPASS_MERCURY_OUTSIDE_TCP_PORT=${ENCOMPASS_MERCURY_OUTSIDE_TCP_PORT} \
+  -e ENCOMPASS_MERCURY_OUTSIDE_SSL_PORT=${ENCOMPASS_MERCURY_OUTSIDE_SSL_PORT} \
+  -e ENCOMPASS_MERCURY_IRCNICK=${ENCOMPASS_MERCURY_IRCNICK} \
+  -e ENCOMPASS_MERCURY_BLOCK_CACHE_SIZE=${ENCOMPASS_MERCURY_BLOCK_CACHE_SIZE} \
+  -e ENCOMPASS_MERCURY_DONATION_ADDR=${ENCOMPASS_MERCURY_DONATION_ADDR} \
+  -e ENCOMPASS_MERCURY_REPORT_HOST=${ENCMPASS_MERCURY_REPORT_HOST} \
   ${IMAGE}
 
 }
